6d114052b03ed2dac2e34fe9c052aa09540f1472
Hugo Levy-Falk <hugo@klafyvel.me>
2022-09-14 08:37:26 +0200

    Compatibility with Julia 1.0 ?

--- test/runtests.jl
+++ test/runtests.jl
@@ -2,6 +2,7 @@ using Norg
 using Test
 using Compat
 
+
 @testset "Norg.jl" begin
     @testset "tokens.jl" begin
         @test Norg.Tokens.Token(Norg.Tokens.LineEnding(), 1, 1, "") isa Norg.Tokens.Token
@@ -18,16 +19,16 @@ using Compat
     end
     @testset "scanners.jl" begin
         @test Norg.Scanners.scan('-', Norg.Tokens.Minus(), "-foo") isa Norg.Tokens.Token{Norg.Tokens.Minus}
-        @test Norg.Scanners.scan('-', Norg.Tokens.Minus(), "+foo") |> isnothing
+        @compat @test Norg.Scanners.scan('-', Norg.Tokens.Minus(), "+foo") |> isnothing
         @test Norg.Scanners.scan("\r\n", Norg.Tokens.LineEnding(), "\r\nfoo") isa Norg.Tokens.Token{Norg.Tokens.LineEnding}
-        @test Norg.Scanners.scan("\r\n", Norg.Tokens.LineEnding(), "foo") |> isnothing
+        @compat @test Norg.Scanners.scan("\r\n", Norg.Tokens.LineEnding(), "foo") |> isnothing
         @test Norg.Scanners.scan(["\r\n", "\n"], Norg.Tokens.LineEnding(), "\r\nfoo") isa Norg.Tokens.Token{Norg.Tokens.LineEnding}
-        @test Norg.Scanners.scan(["\r\n", "\n"], Norg.Tokens.LineEnding(), "foo") |> isnothing
+        @compat @test Norg.Scanners.scan(["\r\n", "\n"], Norg.Tokens.LineEnding(), "foo") |> isnothing
         @test Norg.Scanners.scan(Norg.Scanners.LineEnding(), "\r\nfoo") isa Norg.Tokens.Token{Norg.Tokens.LineEnding}
-        @test Norg.Scanners.scan(Norg.Scanners.LineEnding(), "foo") |> isnothing
+        @compat @test Norg.Scanners.scan(Norg.Scanners.LineEnding(), "foo") |> isnothing
         @test Norg.Scanners.scan(Norg.Scanners.Whitespace(), " foo") isa Norg.Tokens.Token{Norg.Tokens.Whitespace}
         @test Norg.Scanners.scan(Norg.Scanners.Whitespace(), String([' ', Char(0x2006)]) * "foo") isa Norg.Tokens.Token{Norg.Tokens.Whitespace}
-        @test Norg.Scanners.scan(Norg.Scanners.Whitespace(), "foo") |> isnothing
+        @compat @test Norg.Scanners.scan(Norg.Scanners.Whitespace(), "foo") |> isnothing
         # TODO: write test for Norg.Scanners.scan(input; line, charnum)
     end
 end
