942bafd3c8466b318c00d35cc36841ef3b920030
Hugo Levy-Falk <hugo@klafyvel.me>
2023-09-19 07:51:37 +0200

    Project.toml formatting cannot be consistent accross all Julia versions. T_T

--- test/code_analysis_tests/test_aqua.jl
+++ test/code_analysis_tests/test_aqua.jl
@@ -1,7 +1,7 @@
 using Aqua
 
 @static if VERSION < v"1.9"
-    Aqua.test_all(Norg, ambiguities=false)
+    Aqua.test_all(Norg, ambiguities=false, project_toml_formatting=false)
 else
     Aqua.test_all(Norg)
 end
